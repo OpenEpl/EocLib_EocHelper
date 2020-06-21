69275dd93a413563785a20642a4bd2e29aa1076b
qiqiworld <1354092549@qq.com>
2020-06-21 13:13:41 +0800

    feature: use new eoc lib system

--- EocHelper/config.cmake
+++ EocHelper/config.cmake
@@ -1,5 +1,6 @@
 if(NOT EocHelper_FOUND)
 	set(EocHelper_FOUND True)
-	set(EocHelper_INCLUDE_DIRS ${CMAKE_CURRENT_LIST_DIR}/src)
-	set(EocHelper_LIBRARIES)
+	set(EocHelper_INCLUDE_DIRS "")
+	set(EocHelper_LIBRARIES EocHelper)
+	add_subdirectory(${CMAKE_CURRENT_LIST_DIR} ${CMAKE_BINARY_DIR}/EocHelper)
 endif()
